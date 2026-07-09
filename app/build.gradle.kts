8fdb7b8b0741e416c3429d65d2da6b348ba9e140
Viera97 <d.viera1997@gmail.com>
2026-07-09 15:30:26 -0400

    chore: add No_Commit/ to gitignore and bump to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
         applicationId = "com.vida.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
     }
 
     buildTypes {
